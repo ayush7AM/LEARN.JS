c0e8f2af0faeeb4cf3724eca0a033b41e2a61811
AYUSH MISHRA <fightayush@gmail.com>
2024-10-24 19:30:21 +0000

    container config done

---
+++ Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12408218
+var accountName = "Ayush"
+let accountCity = "Gorakhpur"
+accountPass = "chalnachutiye7"
+let accountState 
+
+// cant take accountId as again as it is a const
+// accountId = 124
+console.log(accountId)
+console.table([accountId,accountCity,accountName,accountState])
+/* try not to use var instead use let because of functionality of scope! */
