cb695ba41e0502a14c50bea133f7b92bcae0907a
AYUSH MISHRA <fightayush@gmail.com>
2024-10-30 19:57:23 +0000

    container config done

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treats all JS as newer version
+
+//alert(3+3) //we are using nodeJS not browser
+
+let name ="Ayush"  //string 
+let num = 3   //number
+let isLoggedIn = false  //boolean
+let state = null;//undefined
+//null => standalone value
+//symbol => unique
+//object
+console.log(typeof state) //null is an object data type
