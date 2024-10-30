303fa7e5026243104f3df2805d78c8f593950b00
AYUSH MISHRA <fightayush@gmail.com>
2024-10-30 21:35:47 +0000

    container config done

---
+++ Basics/dataTypesSummary.js
@@ -0,0 +1,28 @@
+// Primitive Data Types:
+// 1 - String
+// 2 - Number
+// 3 - Boolean
+// 4 - undefined
+// 5 - Symbol
+// 6 - BigInt
+// 7 - Null
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id===anotherId)
+
+// Reference(Non Data Types)
+// Array,Object,Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj={
+    name: "Ayush",
+    age: 19    
+}
+const myFunction = function(){
+    console.log("Hello Wolrd");
+}
+
+// console.log(typeof heros)
