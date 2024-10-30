cd3b24b0abe6263075c1280a245673c86a23d02c
AYUSH MISHRA <fightayush@gmail.com>
2024-10-30 20:25:12 +0000

    container config done

---
+++ Basics/03_containerOperations.js
@@ -0,0 +1,17 @@
+// let score = "33abc"
+// let score = null
+// let score = true
+let score = undefined
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  //Remember NaN means Not a Number
+// let isLoggedIn = ""
+// let isLoggedIn = 1
+let isLoggedIn = "Ayush"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn)
+
+//  1 => True //0 => False
+// "" => False // "Ayush" = True
