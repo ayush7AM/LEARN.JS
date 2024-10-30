c9a8db9295e8dcf03aa9d5dbaa4b2bdf63f2190d
AYUSH MISHRA <fightayush@gmail.com>
2024-10-30 20:47:26 +0000

    container config done

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+// let score = null
+// let score = true
+let score = undefined
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  //Remember NaN means Not a Number
+// let isLoggedIn = ""
+// let isLoggedIn = 1
+let isLoggedIn = "Ayush"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn)
+
+//  1 => True //0 => False
+// "" => False // "Ayush" = True
+
+
+// *****************Operations****************
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log("1"+2) //result:12
+// console.log(1+"2") //result:12
+// console.log("1"+2 + 2) //whatever type comes first the whole of the output will be treated on that basic like here result: 122
+// console.log(1+2+ "2") // result: 32
+
+console.log(+true)
+console.log(+"")
+
+let v = 100
+++v //prefix
+// v++ //postfix //Read more about(prefix and postfix)js on MDN
+console.log(v)
